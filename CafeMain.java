2ae3b6ed099f8be837b9b4b7ce7a61e38b863c9f
박태용 <86910922+ParkTaeYonggg@users.noreply.github.com>
2021-07-30 09:46:22 +0900

    Add files via upload

---
+++ CafeMain.java
@@ -0,0 +1,11 @@
+package com.Scanner;
+
+public class CafeMain {
+    public static void main(String[] args) {
+        MyCaffe my = new MyCaffe();
+        
+        my.caffeOpen();
+        my.info();
+        my.menu();
+    }
+}
